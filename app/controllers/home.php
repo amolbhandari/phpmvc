4af0947bf2f75da1c55129484abda0eb0a57c06b
Prajyot Khandeparkar <prajyotpro@gmail.com>
2015-08-25 19:55:58 +0530

    added models - inprogress

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,9 +4,13 @@
 */
 class Home extends Controller
 {
+	public function __construct() {
+		$this->usesModel(array('User'));
+	}
 	
 	public function index($name = '') {
-		echo "home/index ".$name;
+		$this->model['User']->setName("Prajyot");
+		echo $this->model['User']->getName();
 	}
 
 }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,5 +4,14 @@
 */
 class Controller
 {
+	protected $model;
 
+	protected function usesModel($models) {
+		$result = array();
+		foreach ($models as $model) {
+			require_once '../app/models/' . $model . '.php';
+			$result[$model] = new $model();
+		}
+		$this->model = $result;
+	}
 }
